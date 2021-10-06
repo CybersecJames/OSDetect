1c7cd16fda94935327885ba48370b8bd4ed7e829
James Montrief <james.montrief@gmail.com>
2021-10-06 12:04:52 -0400

    code cleanup

--- os_detect.sh
+++ os_detect.sh
@@ -5,24 +5,30 @@
 # Description: OSDect will distinguish between MS windows, MacOS, and Linux operating systems.
 
 clear
+function OSDetect {
+    # detect OS
+    if type -t wevutil &> /dev/null
+    then OS=MS_Windows
+    
+    elif type -t scutil &> /dev/null
+    then OS=MacOS
+    
+    else OS=Linux
+    fi
 
-if type -t wevutil &> /dev/null
-then 
-    OS=MS_Windows
-elif type -t scutil &> /dev/null
-then
-    OS=MacOS
-else
-    OS=Linux
-fi
+    # set variables to display
+    kernelRelease=$(uname -r)
+    arch=$(lscpu | grep 'Architecture' |awk '{print $2}' | head -n 1)
+    name=$(hostname)
 
-kernelRelease=$(uname -r)
+    # display
+    echo Report:
+    echo ------------------------------------------------------------
+    echo Hostname: "$name"
+    echo OS: "$OS"
+    echo Kernel: "${kernelRelease}"
+    echo Architecture: "${arch}"
+    echo ------------------------------------------------------------
+    }
 
-
-arch=$(lscpu | grep 'Architecture' |awk '{print $2}' | head -n 1)
-
-echo ------------------------------------------------------------
-echo Operating system:  $OS 
-echo Kernel: "${kernelRelease}"
-echo Architecture: "${arch}"
-echo ------------------------------------------------------------
+OSDetect
